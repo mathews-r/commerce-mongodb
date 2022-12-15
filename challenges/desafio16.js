99399868ae9307c9a31727550985423ac3cab2bb
Mathews <mathewsrodrigues9@gmail.com>
2022-12-15 17:47:20 -0300

    desafio 18 concluido

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,2 +1,8 @@
-db.produtos.updateMany({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } });
-db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: true, _id: false });
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $set: { ultimaModificacao: new Date() } },
+);
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { nome: true, _id: false },
+);
