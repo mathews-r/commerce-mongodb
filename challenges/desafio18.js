1a801df3d10bb9a5007a10caa541c0c5eba4c0c5
Mathews <mathewsrodrigues9@gmail.com>
2022-12-15 18:00:36 -0300

    desafio 19 concluido

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -2,5 +2,4 @@ db.produtos.updateMany(
   { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
   { $addToSet: { ingredientes: "bacon" } },
 );
-db.produtos.find({},
-  { nome: true, ingredientes: true, _id: false });
+db.produtos.find({}, { nome: true, ingredientes: true, _id: false });
